1e872136f2715198f51306a468571674a800dbd1
emma <emmatone7@gmail.com>
2022-12-18 16:12:38 +0900

    Initial Commit

---
+++ emmaKadai3/emmaKadai3App.swift
@@ -0,0 +1,17 @@
+//
+//  emmaKadai3App.swift
+//  emmaKadai3
+//
+//  Created by Hitomi Ishiodori on 2022/12/18.
+//
+
+import SwiftUI
+
+@main
+struct emmaKadai3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
